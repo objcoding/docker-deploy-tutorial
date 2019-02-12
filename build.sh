61e50b5f866c279f581acaa889345c1fb79876b6
zhangchenghui <zhangchenghui.dev@gmail.com>
2019-02-12 17:02:23 +0800

    docker jenkins

--- build.sh
+++ build.sh
@@ -7,11 +7,11 @@ timestamp=`date +%Y%m%d%H%M%S`
 servicename=docker-jenkins-sample
 
 # build
-echo "开始构建：$registry/$servicename:$active-$timestamp"
+echo "构建镜像：$registry/$servicename:$active-$timestamp"
 docker build --build-arg ACTIVE=${active} -t ${registry}/${servicename}:${active}-${timestamp} .
 echo "上传镜像：$registry/$servicename:$active-$timestamp"
 docker push ${registry}/${servicename}:${active}-${timestamp}
-echo "开始标记版本为$active镜像：$registry/$servicename:$active-$timestamp"
+echo "标记镜像：$registry/$servicename:$active"
 docker tag ${registry}/${servicename}:${active}-${timestamp} ${registry}/${servicename}:${active}
 docker push ${registry}/${servicename}:${active}
 echo "构建完成！"
